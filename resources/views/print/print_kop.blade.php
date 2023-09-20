bb0b1e799fade62263a649654075d6c3675d8cd7
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-09-20 07:42:10 +0700

    perbaikan print kop

---
+++ resources/views/print/print_kop.blade.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <img src="{{ asset('img/bpjs.png') }}" style="height: 70px">
+    <div class="col text-center">
+        <b>KLINIK UTAMA LUTHFI MEDICAL CENTER</b><br>
+        Jl. Raya Sunan Gunung Jati No. 100 A/B Desa Pasindangan Kec. Gunungjati Kab. Cirebon Jawa Barat
+        45151<br>
+        www.luthfimedicalcenter.com - Whatasapp 0823 1169 6919 - Call Center (0231) 8850943
+    </div>
+    <img src="{{ asset('medicio/assets/img/lmc.png') }}" style="height: 70px">
+    <hr width="100%" hight="30px" class="m-1 " color="black" size="50px" />
+</div>
