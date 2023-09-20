331e5f1f2d034467d724dc78955ac0431f08e9b6
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-09-20 07:36:59 +0700

    perbaikan kop asesmen

--- resources/views/form/asesmen_dokter_rajal.blade.php
+++ resources/views/form/asesmen_dokter_rajal.blade.php
@@ -1,31 +1,7 @@
 <section>
     <div class="col-md-12">
         <div class="row">
-            <div class="col-md-2 border border-dark">
-                <div class="m-2  text-center">
-                    <img class="" src="{{ asset('logo3.png') }}" style="height: 80px">
-                </div>
-            </div>
-            <div class="col-md-6  border border-dark">
-                <b>RS</b><br>
-                Jl. Raya Sunan Gunung Jati No. 100 A/B <br>
-                Desa Pasindangan Kec. Gunungjati Kab. Cirebon Jawa Barat 45151<br>
-                www.luthfimedicalcenter.com - Whatasapp 0823 1169 6919 -
-                Call Center (0231) 8850943
-            </div>
-            <div class="col-md-4  border border-dark">
-                <div class="row">
-                    <div class="p-2">
-                        {!! QrCode::size(70)->generate($kunjungan->norm) !!}
-                    </div>
-                    <div>
-                        No RM : <b>{{ $kunjungan->norm }}</b> <br>
-                        Nama : <b>{{ $kunjungan->nama }}</b> <br>
-                        Tgl Lahir : <b>{{ $kunjungan->tgl_lahir }}</b> <br>
-                        Kelamin : <b>{{ $kunjungan->gender }}</b>
-                    </div>
-                </div>
-            </div>
+            @include('form.assesmen_kop')
             <div class="text-center col-md-12 border border-dark bg-warning">
                 <div class="m-2 ">
                     <b>ASESMEN DOKTER RAWAT JALAN</b>
---
+++ resources/views/form/assesmen_kop.blade.php
@@ -0,0 +1,25 @@
+<div class="col-md-2 border border-dark">
+    <div class="m-2  text-center">
+        <img class="" src="{{ asset('medicio/assets/img/lmc.png') }}" style="height: 80px">
+    </div>
+</div>
+<div class="col-md-6  border border-dark">
+    <b>KLINIK UTAMA LUTHFI MEDICAL CENTER</b><br>
+    Jl. Raya Sunan Gunung Jati No. 100 A/B <br>
+    Desa Pasindangan Kec. Gunungjati Kab. Cirebon Jawa Barat 45151<br>
+    www.luthfimedicalcenter.com - Whatasapp 0823 1169 6919 -
+    Call Center (0231) 8850943
+</div>
+<div class="col-md-4  border border-dark">
+    <div class="row">
+        <div class="p-2">
+            {!! QrCode::size(70)->generate($kunjungan->norm) !!}
+        </div>
+        <div>
+            No RM : <b>{{ $kunjungan->norm }}</b> <br>
+            Nama : <b>{{ $kunjungan->nama }}</b> <br>
+            Tgl Lahir : <b>{{ $kunjungan->tgl_lahir }}</b> <br>
+            Kelamin : <b>{{ $kunjungan->gender }}</b>
+        </div>
+    </div>
+</div>
