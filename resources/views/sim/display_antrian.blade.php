6685de29748e12ada9685742c03aa3b7725ea49e
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-09-21 08:01:34 +0700

    gambar

--- resources/views/sim/display_antrian.blade.php
+++ resources/views/sim/display_antrian.blade.php
@@ -36,29 +36,48 @@
                         </ol>
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-                                <img class="d-block w-100" src="{{ asset('medicio/assets/img/slide/slide-1.jpg') }}"
-                                    alt="First slide">
+                                <img class="d-block w-100" src="{{ asset('img/1.jpg') }}" alt="First slide">
                                 <div class="carousel-caption d-none d-md-block">
                                     <h5>...</h5>
                                     <p>...</p>
                                 </div>
                             </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="{{ asset('medicio/assets/img/slide/slide-2.jpg') }}"
-                                    alt="Second slide">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset('img/2.jpg') }}" alt="First slide">
                                 <div class="carousel-caption d-none d-md-block">
                                     <h5>...</h5>
                                     <p>...</p>
                                 </div>
                             </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="{{ asset('medicio/assets/img/slide/slide-3.jpg') }}"
-                                    alt="Third slide">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset('img/3.jpg') }}" alt="First slide">
                                 <div class="carousel-caption d-none d-md-block">
                                     <h5>...</h5>
                                     <p>...</p>
                                 </div>
                             </div>
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset('img/4.jpg') }}" alt="First slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>...</h5>
+                                    <p>...</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset('img/5.jpg') }}" alt="First slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>...</h5>
+                                    <p>...</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset('img/6.jpg') }}" alt="First slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>...</h5>
+                                    <p>...</p>
+                                </div>
+                            </div>
+
                         </div>
                         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
