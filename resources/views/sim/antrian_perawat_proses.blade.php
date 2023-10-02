22ac88794a16edb779a0ccaa524eea02b92954a2
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-10-02 08:54:29 +0700

    perbaikan file penunjnga

--- resources/views/sim/antrian_perawat_proses.blade.php
+++ resources/views/sim/antrian_perawat_proses.blade.php
@@ -256,19 +256,21 @@
                                     icon="fas fa-upload" label="Upload" />
                             </form>
                             @if ($antrian->pasien)
-                                <hr>
-                                @foreach ($antrian->pasien->fileuploads as $file)
-                                    <x-adminlte-card title="{{ $file->nama }} {{ $file->created_at }}" theme="info"
-                                        icon="fas fa-file" collapsible="collapsed">
-                                        <a href="{{ $file->fileurl }}" target="_blank"
-                                            class="btn btn-xs btn-primary mr-1 mb-1">Donwload</a>
-                                        <a href="{{ route('hapusfilepenunjang') }}?id={{ $file->id }}"
-                                            class="btn btn-xs btn-danger mr-1 mb-1">Hapus File</a>
-                                        Diupload pada tanggal : {{ $file->created_at }}
-                                        <br>
-                                        <object data="{{ $file->fileurl }}" width="100%" height="700px"> </object>
-                                    </x-adminlte-card>
-                                @endforeach
+                                @if ($antrian->pasien->fileuploads)
+                                    <hr>
+                                    @foreach ($antrian->pasien->fileuploads as $file)
+                                        <x-adminlte-card title="{{ $file->nama }} {{ $file->created_at }}"
+                                            theme="info" icon="fas fa-file" collapsible="collapsed">
+                                            <a href="{{ $file->fileurl }}" target="_blank"
+                                                class="btn btn-xs btn-primary mr-1 mb-1">Donwload</a>
+                                            <a href="{{ route('hapusfilepenunjang') }}?id={{ $file->id }}"
+                                                class="btn btn-xs btn-danger mr-1 mb-1">Hapus File</a>
+                                            Diupload pada tanggal : {{ $file->created_at }}
+                                            <br>
+                                            <object data="{{ $file->fileurl }}" width="100%" height="700px"> </object>
+                                        </x-adminlte-card>
+                                    @endforeach
+                                @endif
                             @else
                                 <hr>
                                 @foreach ($antrian->fileuploads as $file)
