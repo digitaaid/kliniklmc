5896d7f8a23760e489f3e8a03570fb65cc8cec45
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-10-30 08:20:39 +0700

    perbaikan edit antrian

--- app/Http/Controllers/PendaftaranController.php
+++ app/Http/Controllers/PendaftaranController.php
@@ -251,55 +251,53 @@ class PendaftaranController extends APIController
         $request['status'] = 0;
         $api = new AntrianController();
         $antrian->update($request->all());
-        try {
-            $wapi = new WhatsappController();
-            switch ($request->jeniskunjungan) {
-                case 1:
-                    $jeniskunjungan = "Rujukan FKTP";
-                    break;
-
-                case 2:
-                    $jeniskunjungan = "Umum";
-                    break;
-
-                case 3:
-                    $jeniskunjungan = "Surat Kontrol";
-                    break;
-
-                case 4:
-                    $jeniskunjungan = "Rujukan Antar RS";
-                    break;
-
-                default:
-                    $jeniskunjungan = "-";
-                    break;
-            }
-            // notif pasien
-            $request['keterangan'] = "Silahkan menunggu untuk mendapatkan pelayanan. (TIKET MOHON TIDAK HILANG SAMPAI DENGAN SELESAI PELAYANAN)";
-            $request['message'] = "*Pendaftaran Berhasil*\nAntrian anda berhasil didaftarkan ke sistem KLINIK LMC dengan data sebagai berikut : \n\n*Kode Antrian :* " . $request->kodebooking .  "\n*Angka Antrian :* " . $request->angkaantrean .  "\n*Nomor Antrian :* " . $request->nomorantrean . "\n*Jenis Pasien :* " . $request->jenispasien .  "\n*Jenis Kunjungan :* " . $jeniskunjungan .  "\n\n*Nama :* " . $request->nama . "\n*Poliklinik :* " . $request->namapoli  . "\n*Dokter :* " . $request->namadokter  .  "\n*Jam Praktek :* " . $request->jampraktek  .  "\n*Tanggal Periksa :* " . $request->tanggalperiksa . "\n\n*Keterangan :* " . $request->keterangan  .  "\n\nLink Kodebooking QR Code :\nhttps://luthfimedicalcenter.com/statusantrian?kodebooking=" . $request->kodebooking . "\n\nTerima kasih. \nSalam Hangat dan Sehat Selalu.\nUntuk pertanyaan & pengaduan silahkan hubungi :\n*Customer Care KLINIK LMC (0231)8850943 / 0823 1169 6919*";
-            $request['number'] = $request->nohp;
-            $wapi->send_message($request);
-            // sholawat
-            $sholawat = "اَللّٰهُمَّ صَلِّ عَلٰى سَيِّدِنَا مُحَمَّدٍ، طِبِّ الْقُلُوْبِ وَدَوَائِهَا، وَعَافِيَةِ الْاَبْدَانِ وَشِفَائِهَا، وَنُوْرِ الْاَبْصَارِ وَضِيَائِهَا، وَعَلٰى اٰلِهِ وَصَحْبِهِ وَسَلِّمْ";
-            $request['message'] = $sholawat;
-            $request['number'] = '6289529909036@c.us';
-            $wapi->send_message($request);
-            // notif group
-            $request['message'] = "Berhasil integrasi antrian \nAngka antrian : " . $request->angkaantrean . "\nKodebooking : " . $request->kodebooking .  "\nJenis Pasien : " . $request->jenispasien . "\nNama " . $request->nama . "\nTanggal Periksa " . $request->tanggalperiksa . "\nDokter : " . $request->namadokter . "\nUser : " . Auth::user()->name;
-            $request['number'] = "120363170262520539";
-            $wapi->send_message_group($request);
-        } catch (\Throwable $th) {
-            //throw $th;
-        }
         $res =  $api->tambah_antrean($request);
-        // if ($res->metadata->code == 200) {
+        if ($res->metadata->code == 200) {
+            try {
+                $wapi = new WhatsappController();
+                switch ($request->jeniskunjungan) {
+                    case 1:
+                        $jeniskunjungan = "Rujukan FKTP";
+                        break;
+
+                    case 2:
+                        $jeniskunjungan = "Umum";
+                        break;
+
+                    case 3:
+                        $jeniskunjungan = "Surat Kontrol";
+                        break;
+
+                    case 4:
+                        $jeniskunjungan = "Rujukan Antar RS";
+                        break;
+
+                    default:
+                        $jeniskunjungan = "-";
+                        break;
+                }
+                // notif pasien
+                $request['keterangan'] = "Silahkan menunggu untuk mendapatkan pelayanan. (TIKET MOHON TIDAK HILANG SAMPAI DENGAN SELESAI PELAYANAN)";
+                $request['message'] = "*Pendaftaran Berhasil*\nAntrian anda berhasil didaftarkan ke sistem KLINIK LMC dengan data sebagai berikut : \n\n*Kode Antrian :* " . $request->kodebooking .  "\n*Angka Antrian :* " . $request->angkaantrean .  "\n*Nomor Antrian :* " . $request->nomorantrean . "\n*Jenis Pasien :* " . $request->jenispasien .  "\n*Jenis Kunjungan :* " . $jeniskunjungan .  "\n\n*Nama :* " . $request->nama . "\n*Poliklinik :* " . $request->namapoli  . "\n*Dokter :* " . $request->namadokter  .  "\n*Jam Praktek :* " . $request->jampraktek  .  "\n*Tanggal Periksa :* " . $request->tanggalperiksa . "\n\n*Keterangan :* " . $request->keterangan  .  "\n\nLink Kodebooking QR Code :\nhttps://luthfimedicalcenter.com/statusantrian?kodebooking=" . $request->kodebooking . "\n\nTerima kasih. \nSalam Hangat dan Sehat Selalu.\nUntuk pertanyaan & pengaduan silahkan hubungi :\n*Customer Care KLINIK LMC (0231)8850943 / 0823 1169 6919*";
+                $request['number'] = $request->nohp;
+                $wapi->send_message($request);
+                // sholawat
+                $sholawat = "اَللّٰهُمَّ صَلِّ عَلٰى سَيِّدِنَا مُحَمَّدٍ، طِبِّ الْقُلُوْبِ وَدَوَائِهَا، وَعَافِيَةِ الْاَبْدَانِ وَشِفَائِهَا، وَنُوْرِ الْاَبْصَارِ وَضِيَائِهَا، وَعَلٰى اٰلِهِ وَصَحْبِهِ وَسَلِّمْ";
+                $request['message'] = $sholawat;
+                $request['number'] = '6289529909036@c.us';
+                $wapi->send_message($request);
+                // notif group
+                $request['message'] = "Berhasil integrasi antrian \nAngka antrian : " . $request->angkaantrean . "\nKodebooking : " . $request->kodebooking .  "\nJenis Pasien : " . $request->jenispasien . "\nNama " . $request->nama . "\nTanggal Periksa " . $request->tanggalperiksa . "\nDokter : " . $request->namadokter . "\nUser : " . Auth::user()->name;
+                $request['number'] = "120363170262520539";
+                $wapi->send_message_group($request);
+            } catch (\Throwable $th) {
+                //throw $th;
+            }
+        }
         $antrian->update([
             'status' => 1
         ]);
         Alert::success('Success', 'Antrian telah diperbaharui.');
-        // } else {
-        //     Alert::error('Mohon Maaf', $res->metadata->message);
-        // }
         return redirect()->back();
     }
     function editkunjungan(Request $request)
