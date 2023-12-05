2e3e2dba0161f37f0bdb8668859b2bd4b04b769d
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-12-05 09:16:40 +0700

    scrun

--- app/Http/Controllers/SyncronizeController.php
+++ app/Http/Controllers/SyncronizeController.php
@@ -13,43 +13,49 @@ class SyncronizeController extends Controller
     {
         $antrians = null;
         $api = new AntrianController();
-        $antrian = Antrian::where('taskid', 7)->where('status', 0)->first();
-        if ($antrian) {
-            try {
-                $request['kodebooking'] = $antrian->kodebooking;
-                $request['taskid'] = 1;
-                $request['waktu'] = Carbon::parse($antrian->taskid1, 'Asia/Jakarta');
-                $res =  $api->update_antrean($request);
-                $request['taskid'] = 2;
-                $request['waktu'] = Carbon::parse($antrian->taskid2, 'Asia/Jakarta');
-                $res =  $api->update_antrean($request);
-                $request['taskid'] = 3;
-                $request['waktu'] = Carbon::parse($antrian->taskid4, 'Asia/Jakarta')->subMinutes(random_int(4, 9));
-                $res = $api->update_antrean($request);
-                $request['taskid'] = 4;
-                $request['waktu'] = Carbon::parse($antrian->taskid4, 'Asia/Jakarta');
-                $res = $api->update_antrean($request);
-                $request['taskid'] = 5;
-                $request['waktu'] = Carbon::parse($antrian->taskid5, 'Asia/Jakarta');
-                $res =  $api->update_antrean($request);
-                $request['taskid'] = 6;
-                $request['waktu'] = Carbon::parse($antrian->taskid6, 'Asia/Jakarta');
-                $res =  $api->update_antrean($request);
-                $request['taskid'] = 7;
-                $request['waktu'] = Carbon::parse($antrian->taskid7, 'Asia/Jakarta');
-                $res =  $api->update_antrean($request);
-                dd($res);
-                if ($res->metadata->code == 200 || $res->metadata->message == "TaskId=7 sudah ada") {
-                    $antrian->update([
-                        'status' => 1
-                    ]);
+        if ($request->tanggal) {
+            $tanggal = explode('-', $request->tanggal);
+            $request['tanggalawal'] = Carbon::parse($tanggal[0])->format('Y-m-d');
+            $request['tanggalakhir'] = Carbon::parse($tanggal[1])->format('Y-m-d');
+            $antrian = Antrian::whereBetween('tanggalperiksa', [$request->tanggalawal, $request->tanggalakhir])
+                ->where('taskid', 7)->where('status', 0)->first();
+            if ($antrian) {
+                try {
+                    $request['kodebooking'] = $antrian->kodebooking;
+                    $request['taskid'] = 1;
+                    $request['waktu'] = Carbon::parse($antrian->taskid1, 'Asia/Jakarta');
+                    $res =  $api->update_antrean($request);
+                    $request['taskid'] = 2;
+                    $request['waktu'] = Carbon::parse($antrian->taskid2, 'Asia/Jakarta');
+                    $res =  $api->update_antrean($request);
+                    $request['taskid'] = 3;
+                    $request['waktu'] = Carbon::parse($antrian->taskid4, 'Asia/Jakarta')->subMinutes(random_int(4, 9));
+                    $res = $api->update_antrean($request);
+                    $request['taskid'] = 4;
+                    $request['waktu'] = Carbon::parse($antrian->taskid4, 'Asia/Jakarta');
+                    $res = $api->update_antrean($request);
+                    $request['taskid'] = 5;
+                    $request['waktu'] = Carbon::parse($antrian->taskid5, 'Asia/Jakarta');
+                    $res =  $api->update_antrean($request);
+                    $request['taskid'] = 6;
+                    $request['waktu'] = Carbon::parse($antrian->taskid6, 'Asia/Jakarta');
+                    $res =  $api->update_antrean($request);
+                    $request['taskid'] = 7;
+                    $request['waktu'] = Carbon::parse($antrian->taskid7, 'Asia/Jakarta');
+                    $res =  $api->update_antrean($request);
+                    if ($res->metadata->code == 200 || $res->metadata->message == "TaskId=7 sudah ada") {
+                        $antrian->update([
+                            'status' => 1
+                        ]);
+                    }
+                    Alert::success('Success', 'Berhasil snyc');
+                } catch (\Throwable $th) {
+                    Alert::error('Error', $th->getMessage());
                 }
-                Alert::success('Success', 'Berhasil snyc');
-            } catch (\Throwable $th) {
-                Alert::error('Error', $th->getMessage());
             }
+            $antrians = Antrian::whereBetween('tanggalperiksa', [$request->tanggalawal, $request->tanggalakhir])
+                ->where('taskid', 7)->get();
         }
-        $antrians = Antrian::where('taskid', 7)->where('status', 0)->get();
         return view('sim.sync_antrian_bpjs', compact([
             'request',
             'antrians',
