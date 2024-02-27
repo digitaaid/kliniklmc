4fd3c0b82a202cac347507724913e34935973fe3
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2024-02-27 11:42:58 +0700

    resume rajal

--- resources/views/sim/modal_resume_rajal.blade.php
+++ resources/views/sim/modal_resume_rajal.blade.php
@@ -1,6 +1,6 @@
 <x-adminlte-modal id="modalResumeRajal" title="Resume Rawat" size="xl" icon="fas fa-hand-holding-medical"
     theme="success">
-
+    @include('form.resume_rajal')
     <x-slot name="footerSlot">
         <a href="{{ route('print_resume_rajal') }}?kodekunjungan={{ $antrian->kunjungan->kode }}"
             class="btn btn-warning ml-auto" target="_blank"> <i class="fas fa-print"></i> Print</a>
