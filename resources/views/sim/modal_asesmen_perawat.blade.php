a7cdeb003fab528bed0dc9ac1b17c3d427a20305
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2024-03-02 17:21:43 +0700

    hilang required

--- resources/views/sim/modal_asesmen_perawat.blade.php
+++ resources/views/sim/modal_asesmen_perawat.blade.php
@@ -19,29 +19,29 @@
                         {{ $antrian->asesmenperawat ? ($antrian->asesmenperawat->sumber_data == 'Keluarga / Alloanamnesa' ? 'selected' : null) : null }}>
                         Keluarga / Alloanamnesa</option>
                 </x-adminlte-select>
-                <x-adminlte-textarea required igroup-size="sm" rows=3 label="Keluhan Utama" name="keluhan_utama"
+                <x-adminlte-textarea  igroup-size="sm" rows=3 label="Keluhan Utama" name="keluhan_utama"
                     placeholder="Keluhan Utama">
                     {{ $antrian->asesmenperawat->keluhan_utama ?? null }}
                 </x-adminlte-textarea>
                 <div class="row">
                     <div class="col-md-6">
-                        <x-adminlte-textarea required igroup-size="sm" rows=3 label="Riwayat Penyakit"
+                        <x-adminlte-textarea  igroup-size="sm" rows=3 label="Riwayat Penyakit"
                             name="riwayat_penyakit" placeholder="Riwayat Penyakit">
                             {{ $antrian->asesmenperawat->riwayat_penyakit ?? null }}
                         </x-adminlte-textarea>
                     </div>
                     <div class="col-md-6">
-                        <x-adminlte-textarea required igroup-size="sm" rows=3 label="Riwayat Penyakit Keluarga"
+                        <x-adminlte-textarea  igroup-size="sm" rows=3 label="Riwayat Penyakit Keluarga"
                             name="riwayat_penyakit_keluarga" placeholder="Riwayat Penyakit">
                             {{ $antrian->asesmenperawat->riwayat_penyakit_keluarga ?? null }}
                         </x-adminlte-textarea>
                     </div>
                 </div>
-                <x-adminlte-textarea required igroup-size="sm" rows=3 label="Riwayat Alergi" name="riwayat_alergi"
+                <x-adminlte-textarea  igroup-size="sm" rows=3 label="Riwayat Alergi" name="riwayat_alergi"
                     placeholder="Riwayat Alergi">
                     {{ $antrian->asesmenperawat->riwayat_alergi ?? null }}
                 </x-adminlte-textarea>
-                <x-adminlte-textarea required igroup-size="sm" rows=3 label="Riwayat Pengobatan"
+                <x-adminlte-textarea  igroup-size="sm" rows=3 label="Riwayat Pengobatan"
                     name="riwayat_pengobatan" placeholder="Riwayat Pengobatan">
                     {{ $antrian->asesmenperawat->riwayat_pengobatan ?? null }}
                 </x-adminlte-textarea>
@@ -53,11 +53,11 @@
                         <label for="skala_nyeri">Skala Nyeri</label>
                     </div>
                     <div class="col-md-2">
-                        <x-adminlte-input required name="skala_nyeri" type="number" placeholder="Skala"
+                        <x-adminlte-input  name="skala_nyeri" type="number" placeholder="Skala"
                             value="{{ $antrian->asesmenperawat->skala_nyeri ?? null }}" />
                     </div>
                     <div class="col-md-7">
-                        <x-adminlte-input required name="keluhan_nyeri" placeholder="Keluhan Nyeri"
+                        <x-adminlte-input  name="keluhan_nyeri" placeholder="Keluhan Nyeri"
                             value="{{ $antrian->asesmenperawat->keluhan_nyeri ?? null }}" />
                     </div>
                 </div>
@@ -86,7 +86,7 @@
                         <label for="resiko_jatuh">Resiko Jatuh</label>
                     </div>
                     <div class="col-md-9">
-                        <x-adminlte-select required name="resiko_jatuh">
+                        <x-adminlte-select  name="resiko_jatuh">
                             <option value="Tidak Bersiko"
                                 {{ $antrian->asesmenperawat ? ($antrian->asesmenperawat->resiko_jatuh == 'Tidak Bersiko' ? 'selected' : null) : null }}>
                                 Tidak Bersiko (tidak ditemukan a dan b)</option>
@@ -104,7 +104,7 @@
                         <label for="alat_bantu">Alat Bantu</label>
                     </div>
                     <div class="col-md-4">
-                        <x-adminlte-select required name="alat_bantu">
+                        <x-adminlte-select  name="alat_bantu">
                             <option
                                 {{ $antrian->asesmenperawat ? ($antrian->asesmenperawat->alat_bantu == 'Tidak Ada' ? 'selected' : null) : null }}>
                                 Tidak
@@ -125,7 +125,7 @@
                         </x-adminlte-select>
                     </div>
                     <div class="col-md-5">
-                        <x-adminlte-input required name="alat_bantu_text" placeholder="Alat Bantu Lainnya"
+                        <x-adminlte-input  name="alat_bantu_text" placeholder="Alat Bantu Lainnya"
                             value="{{ $antrian->asesmenperawat->alat_bantu_text ?? null }}" />
                     </div>
                 </div>
@@ -134,7 +134,7 @@
                         <label for="cacat_fisik">Cacat Fisik</label>
                     </div>
                     <div class="col-md-4">
-                        <x-adminlte-select required name="cacat_fisik">
+                        <x-adminlte-select  name="cacat_fisik">
                             <option
                                 {{ $antrian->asesmenperawat ? ($antrian->asesmenperawat->cacat_fisik == 'Tidak Ada' ? 'selected' : null) : null }}>
                                 Tidak Ada</option>
@@ -145,7 +145,7 @@
                         </x-adminlte-select>
                     </div>
                     <div class="col-md-5">
-                        <x-adminlte-input required name="cacat_fisik_text" placeholder="Cacat Fisik Lainnya"
+                        <x-adminlte-input  name="cacat_fisik_text" placeholder="Cacat Fisik Lainnya"
                             value="{{ $antrian->asesmenperawat->cacat_fisik_text ?? null }}" />
                     </div>
                 </div>
@@ -490,7 +490,7 @@
         <h6>Objective (O) - Tanda Vital</h6>
         <div class="row">
             <div class="col-md-6">
-                <x-adminlte-input required name="denyut_jantung" label="Denyut Jantung" igroup-size="sm"
+                <x-adminlte-input  name="denyut_jantung" label="Denyut Jantung" igroup-size="sm"
                     type="number" placeholder="Denyut Jantung" fgroup-class="row" label-class="text-left col-5"
                     igroup-size="sm" igroup-class="col-7"
                     value="{{ $antrian->asesmenperawat->denyut_jantung ?? null }}">
@@ -500,7 +500,7 @@
                         </div>
                     </x-slot>
                 </x-adminlte-input>
-                <x-adminlte-input required name="pernapasan" label="Pernapasan" igroup-size="sm"
+                <x-adminlte-input  name="pernapasan" label="Pernapasan" igroup-size="sm"
                     placeholder="Pernapasan" fgroup-class="row" label-class="text-left col-5" igroup-size="sm"
                     igroup-class="col-7" type="number" value="{{ $antrian->asesmenperawat->pernapasan ?? null }}">
                     <x-slot name="appendSlot">
@@ -509,7 +509,7 @@
                         </div>
                     </x-slot>
                 </x-adminlte-input>
-                <x-adminlte-input required name="sistole" label="Sistole" igroup-size="sm" placeholder="Sistole"
+                <x-adminlte-input  name="sistole" label="Sistole" igroup-size="sm" placeholder="Sistole"
                     fgroup-class="row" label-class="text-left col-5" igroup-size="sm" igroup-class="col-7"
                     type="number" value="{{ $antrian->asesmenperawat->sistole ?? null }}">
                     <x-slot name="appendSlot">
@@ -518,7 +518,7 @@
                         </div>
                     </x-slot>
                 </x-adminlte-input>
-                <x-adminlte-input required name="distole" label="Diastole" igroup-size="sm" placeholder="Diastole"
+                <x-adminlte-input  name="distole" label="Diastole" igroup-size="sm" placeholder="Diastole"
                     fgroup-class="row" label-class="text-left col-5" igroup-size="sm" igroup-class="col-7"
                     type="number" value="{{ $antrian->asesmenperawat->distole ?? null }}">
                     <x-slot name="appendSlot">
@@ -529,7 +529,7 @@
                 </x-adminlte-input>
             </div>
             <div class="col-md-6">
-                <x-adminlte-input required name="suhu" label="Suhu Tubuh" igroup-size="sm" fgroup-class="row"
+                <x-adminlte-input  name="suhu" label="Suhu Tubuh" igroup-size="sm" fgroup-class="row"
                     label-class="text-left col-5" igroup-size="sm" igroup-class="col-7" placeholder="Suhu Tubuh"
                     value="{{ $antrian->asesmenperawat->suhu ?? null }}">
                     <x-slot name="appendSlot">
@@ -538,7 +538,7 @@
                         </div>
                     </x-slot>
                 </x-adminlte-input>
-                <x-adminlte-input required name="berat_badan" label="Berat Badan" igroup-size="sm"
+                <x-adminlte-input  name="berat_badan" label="Berat Badan" igroup-size="sm"
                     fgroup-class="row" label-class="text-left col-5" igroup-size="sm" igroup-class="col-7"
                     placeholder="Berat Badan" type="number" oninput="indexBsa()"
                     value="{{ $antrian->asesmenperawat->berat_badan ?? null }}">
@@ -548,7 +548,7 @@
                         </div>
                     </x-slot>
                 </x-adminlte-input>
-                <x-adminlte-input required name="tinggi_badan" type="number" label="Tinggi Badan"
+                <x-adminlte-input  name="tinggi_badan" type="number" label="Tinggi Badan"
                     fgroup-class="row" label-class="text-left col-5" igroup-size="sm" igroup-class="col-7"
                     igroup-size="sm" placeholder="Tinggi Badan" oninput="indexBsa()"
                     value="{{ $antrian->asesmenperawat->tinggi_badan ?? null }}">
@@ -602,46 +602,46 @@
                 Confusional States
             </option>
         </x-adminlte-select>
-        <x-adminlte-textarea required igroup-size="sm" rows=4 label="Tanda Vital Tubuh" name="keadaan_tubuh"
+        <x-adminlte-textarea  igroup-size="sm" rows=4 label="Tanda Vital Tubuh" name="keadaan_tubuh"
             placeholder="Tanda Vital Tubuh">
             {{ $antrian->asesmenperawat->keadaan_tubuh ?? null }}
         </x-adminlte-textarea>
         {{-- <h6>Objective (O) - Laboratorium, Radiologi, & Penunjang Lainnya</h6>
         <div class="row">
             <div class="col-md-4">
-                <x-adminlte-textarea required igroup-size="sm" rows=4 label="Hasil Pemeriksaan Laboratorium"
+                <x-adminlte-textarea  igroup-size="sm" rows=4 label="Hasil Pemeriksaan Laboratorium"
                     name="pemeriksaan_lab" placeholder="Hasil Pemeriksaan Laboratorium">
                     {{ $antrian->asesmenperawat->pemeriksaan_lab ?? null }}
                 </x-adminlte-textarea>
             </div>
             <div class="col-md-4">
-                <x-adminlte-textarea required igroup-size="sm" rows=4 label="Hasil Pemeriksaan Radiologi"
+                <x-adminlte-textarea  igroup-size="sm" rows=4 label="Hasil Pemeriksaan Radiologi"
                     name="pemeriksaan_rad" placeholder="Hasil Pemeriksaan Radiologi">
                     {{ $antrian->asesmenperawat->pemeriksaan_rad ?? null }}
                 </x-adminlte-textarea>
             </div>
             <div class="col-md-4">
-                <x-adminlte-textarea required igroup-size="sm" rows=4 label="Hasil Pemeriksaan Penunjang Lainnya"
+                <x-adminlte-textarea  igroup-size="sm" rows=4 label="Hasil Pemeriksaan Penunjang Lainnya"
                     name="pemeriksaan_penunjang" placeholder="Hasil Pemeriksaan Penunjang Lainnya">
                     {{ $antrian->asesmenperawat->pemeriksaan_penunjang ?? null }}
                 </x-adminlte-textarea>
             </div>
         </div> --}}
         <h6>Analysis (A)</h6>
-        <x-adminlte-textarea required igroup-size="sm" rows=3 label="Diagnosa Keperawatan"
+        <x-adminlte-textarea  igroup-size="sm" rows=3 label="Diagnosa Keperawatan"
             name="diagnosa_keperawatan" placeholder="Diagnosa Keperawatan">
             {{ $antrian->asesmenperawat->diagnosa_keperawatan ?? null }}
         </x-adminlte-textarea>
         <h6>Planning (P)</h6>
-        <x-adminlte-textarea required igroup-size="sm" rows=3 label="Rencana Keperawatan" name="rencana_keperawatan"
+        <x-adminlte-textarea  igroup-size="sm" rows=3 label="Rencana Keperawatan" name="rencana_keperawatan"
             placeholder="Rencana Keperawatan">
             {{ $antrian->asesmenperawat->rencana_keperawatan ?? null }}
         </x-adminlte-textarea>
-        <x-adminlte-textarea required igroup-size="sm" rows=3 label="Tindakan Keperawatan"
+        <x-adminlte-textarea  igroup-size="sm" rows=3 label="Tindakan Keperawatan"
             name="tindakan_keperawatan" placeholder="Tindakan Keperawatan">
             {{ $antrian->asesmenperawat->tindakan_keperawatan ?? null }}
         </x-adminlte-textarea>
-        <x-adminlte-textarea required igroup-size="sm" rows=3 label="Evaluasi Keperawatan"
+        <x-adminlte-textarea  igroup-size="sm" rows=3 label="Evaluasi Keperawatan"
             name="evaluasi_keperawatan" placeholder="Evaluasi Keperawatan">
             {{ $antrian->asesmenperawat->evaluasi_keperawatan ?? null }}
         </x-adminlte-textarea>
