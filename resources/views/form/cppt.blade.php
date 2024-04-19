d88e26be697955d5d407a2907c111f1fce661f26
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2024-04-19 15:22:55 +0700

    pasien

--- resources/views/form/cppt.blade.php
+++ resources/views/form/cppt.blade.php
@@ -37,10 +37,10 @@
                     <h5>{{ $pasien->norm ?? '-' }}</h5>
                 </div>
                 <div class="col-md-7 border border-dark text-center">
-                    <h5>{{ $pasien->nama }}</h5>
+                    <h5>{{ $pasien->nama ?? '-' }}</h5>
                 </div>
                 <div class="col-md-3 border border-dark text-center">
-                    {{ $pasien->created_at }}
+                    {{ $pasien->created_at ?? '-'}}
                 </div>
                 <div class="col-md-12 border border-dark">
                     <div class="row">
@@ -49,24 +49,24 @@
                                 <dt class="col-sm-3 m-0">Nama</dt>
                                 <dd class="col-sm-9 m-0">{{ $pasien->nama ?? '-' }}</dd>
                                 <dt class="col-sm-3 m-0">Tgl Lahir</dt>
-                                <dd class="col-sm-9 m-0">{{ $pasien->tgl_lahir ?? '-'}}</dd>
+                                <dd class="col-sm-9 m-0">{{ $pasien->tgl_lahir ?? '-' }}</dd>
                                 <dt class="col-sm-3 m-0">Jns Kelamin</dt>
-                                <dd class="col-sm-9 m-0">{{ $pasien->gender?? '-' }}</dd>
+                                <dd class="col-sm-9 m-0">{{ $pasien->gender ?? '-' }}</dd>
                                 <dt class="col-sm-3 m-0">Alamat</dt>
-                                <dd class="col-sm-9 m-0">{{ $pasien->alamat?? '-' }}</dd>
+                                <dd class="col-sm-9 m-0">{{ $pasien->alamat ?? '-' }}</dd>
 
                             </dl>
                         </div>
                         <div class="col-md-5">
                             <dl class="row">
                                 <dt class="col-sm-3 m-0">No RM</dt>
-                                <dd class="col-sm-9 m-0">{{ $pasien->norm }}</dd>
+                                <dd class="col-sm-9 m-0">{{ $pasien->norm ?? '-' }}</dd>
                                 <dt class="col-sm-3 m-0">NIK</dt>
-                                <dd class="col-sm-9 m-0">{{ $pasien->nik }}</dd>
+                                <dd class="col-sm-9 m-0">{{ $pasien->nik?? '-' }}</dd>
                                 <dt class="col-sm-3 m-0">No BPJS</dt>
-                                <dd class="col-sm-9 m-0">{{ $pasien->nomorkartu }}</dd>
+                                <dd class="col-sm-9 m-0">{{ $pasien->nomorkartu ?? '-'}}</dd>
                                 <dt class="col-sm-3 m-0">No HP</dt>
-                                <dd class="col-sm-9 m-0">{{ $pasien->nohp }}</dd>
+                                <dd class="col-sm-9 m-0">{{ $pasien->nohp ?? '-'}}</dd>
                             </dl>
                         </div>
                         <div class="col-md-4">
